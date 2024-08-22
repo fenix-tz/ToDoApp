751c08db50242a9ec487d353d4d20d9db271ceef
fenix-tz <juantobonz22@gmail.com>
2024-08-22 13:21:54 -0500

    se creo la clase todo con metodo constructor

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,13 @@
 # TODO: Add code here
+# todo.py
+
+from typing import List, Dict
+
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str) -> None:
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: List[str] = []
+
